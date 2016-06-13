d40dc82ff0c85341b64ad62294787f57e746a344
taylorjames9 <taylorjames9@gmail.com>
2016-06-13 13:19:20 -0700

    so cool. added new planets

--- Assets/ForceFieldPro/Demo/Script/ForceOnParticle.cs
+++ Assets/ForceFieldPro/Demo/Script/ForceOnParticle.cs
@@ -8,7 +8,9 @@ using System.Collections;
 public class ForceOnParticle : MonoBehaviour
 {
     [FFToolTip("The force field that will be used.")]
-    public ForceField field;
+    public ForceField field_01;
+	public ForceField field_02;
+	public ForceField field_03;
 
     [FFToolTip("Modifier of the force.")]
     public float factor = 1;
@@ -25,49 +27,102 @@ public class ForceOnParticle : MonoBehaviour
 
     void Update()
     {
-        if (field != null)
-        {
-            foreach (ParticleEmitter pe in pes)
-            {
-                Transform t = pe.transform;
-                Particle[] particles = pe.particles;
-                int num = particles.Length;
-                for (int i = 0; i < num; i++)
-                {
-                    Vector3 force;
-                    if (!pe.useWorldSpace)
-                    {
-                        force = t.InverseTransformDirection(field.GetForce(t.TransformPoint(particles[i].position)));
-                    }
-                    else
-                    {
-                        force = field.GetForce(particles[i].position);
-                    }
-                    particles[i].velocity += force * factor;
-                }
-                pe.particles = particles;
-            }
-            foreach (ParticleSystem ps in pss)
-            {
-                Transform t = ps.transform;
-                ParticleSystem.Particle[] particles = new ParticleSystem.Particle[ps.maxParticles];
-                int num = ps.GetParticles(particles);
-                for (int i = 0; i < num; i++)
-                {
-                    Vector3 force;
-                    if (ps.simulationSpace == ParticleSystemSimulationSpace.Local)
-                    {
-                        force = t.InverseTransformDirection(field.GetForce(t.TransformPoint(particles[i].position)));
-                    }
-                    else
-                    {
-                        force = field.GetForce(particles[i].position);
-                    }
-                    particles[i].velocity += force * factor;
-                }
-                ps.SetParticles(particles, num);
-            }
-        }
+		if (field_01 != null) {
+				foreach (ParticleEmitter pe in pes) {
+					Transform t = pe.transform;
+					Particle[] particles = pe.particles;
+					int num = particles.Length;
+					for (int i = 0; i < num; i++) {
+						Vector3 force;
+						if (!pe.useWorldSpace) {
+						force = t.InverseTransformDirection (field_01.GetForce (t.TransformPoint (particles [i].position)));
+						} else {
+						force = field_01.GetForce (particles [i].position);
+						}
+						particles [i].velocity += force * factor;
+					}
+					pe.particles = particles;
+				}
+				foreach (ParticleSystem ps in pss) {
+					Transform t = ps.transform;
+					ParticleSystem.Particle[] particles = new ParticleSystem.Particle[ps.maxParticles];
+					int num = ps.GetParticles (particles);
+					for (int i = 0; i < num; i++) {
+						Vector3 force;
+						if (ps.simulationSpace == ParticleSystemSimulationSpace.Local) {
+						force = t.InverseTransformDirection (field_01.GetForce (t.TransformPoint (particles [i].position)));
+						} else {
+						force = field_01.GetForce (particles [i].position);
+						}
+						particles [i].velocity += force * factor;
+					}
+					ps.SetParticles (particles, num);
+				}
+			}
+		if (field_02 != null) {
+			foreach (ParticleEmitter pe in pes) {
+				Transform t = pe.transform;
+				Particle[] particles = pe.particles;
+				int num = particles.Length;
+				for (int i = 0; i < num; i++) {
+					Vector3 force;
+					if (!pe.useWorldSpace) {
+						force = t.InverseTransformDirection (field_02.GetForce (t.TransformPoint (particles [i].position)));
+					} else {
+						force = field_02.GetForce (particles [i].position);
+					}
+					particles [i].velocity += force * factor;
+				}
+				pe.particles = particles;
+			}
+			foreach (ParticleSystem ps in pss) {
+				Transform t = ps.transform;
+				ParticleSystem.Particle[] particles = new ParticleSystem.Particle[ps.maxParticles];
+				int num = ps.GetParticles (particles);
+				for (int i = 0; i < num; i++) {
+					Vector3 force;
+					if (ps.simulationSpace == ParticleSystemSimulationSpace.Local) {
+						force = t.InverseTransformDirection (field_02.GetForce (t.TransformPoint (particles [i].position)));
+					} else {
+						force = field_02.GetForce (particles [i].position);
+					}
+					particles [i].velocity += force * factor;
+				}
+				ps.SetParticles (particles, num);
+			}
+		}
+		if (field_03 != null) {
+			foreach (ParticleEmitter pe in pes) {
+				Transform t = pe.transform;
+				Particle[] particles = pe.particles;
+				int num = particles.Length;
+				for (int i = 0; i < num; i++) {
+					Vector3 force;
+					if (!pe.useWorldSpace) {
+						force = t.InverseTransformDirection (field_03.GetForce (t.TransformPoint (particles [i].position)));
+					} else {
+						force = field_03.GetForce (particles [i].position);
+					}
+					particles [i].velocity += force * factor;
+				}
+				pe.particles = particles;
+			}
+			foreach (ParticleSystem ps in pss) {
+				Transform t = ps.transform;
+				ParticleSystem.Particle[] particles = new ParticleSystem.Particle[ps.maxParticles];
+				int num = ps.GetParticles (particles);
+				for (int i = 0; i < num; i++) {
+					Vector3 force;
+					if (ps.simulationSpace == ParticleSystemSimulationSpace.Local) {
+						force = t.InverseTransformDirection (field_03.GetForce (t.TransformPoint (particles [i].position)));
+					} else {
+						force = field_03.GetForce (particles [i].position);
+					}
+					particles [i].velocity += force * factor;
+				}
+				ps.SetParticles (particles, num);
+			}
+		}
     }
 
 }
