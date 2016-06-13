1981ac1b59e67cbb287973e87b03f6d3f770576b
taylorjames9 <taylorjames9@gmail.com>
2016-06-13 14:32:45 -0700

    adding a full screen clickable plane

--- Assets/Scripts/OscillatingSpiralSize.cs
+++ Assets/Scripts/OscillatingSpiralSize.cs
@@ -4,17 +4,18 @@ using System.Collections;
 public class OscillatingSpiralSize : MonoBehaviour {
 
 	int counter;
+	SprialField spf;
+
 
 	// Use this for initialization
 	void Start () {
-	
+		spf = GetComponent<SprialField> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		counter++;
 		if (counter % 100 == 0) {
-			SprialField spf = GetComponent<SprialField> ();
 			spf.size = Mathf.PingPong (Time.time, 3.0f) -1 ;
 		}
 	
