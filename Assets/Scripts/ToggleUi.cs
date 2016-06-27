5f900f7ab3b8b2efa6d0f0595d00c511e7aa46ec
jsngai <justin.ngai@gmail.com>
2016-06-27 15:22:19 -0700

    added ui toggle functionality to planets

---
+++ Assets/Scripts/ToggleUi.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using TouchScript.Behaviors;
+using TouchScript.Gestures;
+using UnityEngine.UI;
+using System;
+
+public class ToggleUi : MonoBehaviour {
+
+	// Use this for initialization
+	void OnEnable()
+	{
+		GetComponent<LongPressGesture> ().LongPressed += togglePlanetUI;	
+	}
+
+	void OnDisable()
+	{
+		GetComponent<LongPressGesture> ().LongPressed += togglePlanetUI;
+	}
+
+	void togglePlanetUI(object sender, EventArgs e)
+	{
+		if (GetComponentInChildren<Canvas> ().enabled) {
+			GetComponentInChildren<Canvas> ().enabled = false;	
+		} else {
+			GetComponentInChildren<Canvas> ().enabled = true;
+		}
+		//Debug.Log (this + " has been pressed");
+	}
+}
