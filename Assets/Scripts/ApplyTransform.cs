c753e9d4ec3b68173bf9ba7484a06825571879fc
jsngai <justin.ngai@gmail.com>
2016-06-29 13:22:56 -0700

    Updated to have new touchscript

---
+++ Assets/Scripts/ApplyTransform.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript;
+using TouchScript.Gestures;
+
+public class ApplyTransform: MonoBehaviour{
+	TransformGesture myTransformGesture;
+
+	void Awake()
+	{
+		myTransformGesture = GetComponent<TransformGesture> ();
+	}
+
+	void OnEnable()
+	{
+		myTransformGesture.Transformed += MyTransformGesture_Transformed;
+	}
+
+	void OnDisable()
+	{
+		myTransformGesture.Transformed -= MyTransformGesture_Transformed;
+
+	}
+
+	void MyTransformGesture_Transformed(object sender, System.EventArgs e)
+	{
+		myTransformGesture.ApplyTransform (this.transform);
+	}
+}
